707bb61b62a8101b05d1b3533565cae0c96edbde
Raihan Noor Hasan <raihanc39@gamil.com>
2022-09-18 20:06:01 +0600

    fixed assertion not supporting to firefox using fetch to load data

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-import data from './data.json' assert { type: 'json' };
+// import data from './data.json' assert { type: 'json' }; // assert not supporting firefox that's why using fetch method to load data.
+
 import editItem from './utilities/editItem.js';
 import renderItem from './utilities/renderItem.js';
 
+async function loadData() {
+    const res = await fetch("/frontend-mentor-challenges/time-tracking-dashboard/js/data.json");
+    return await res.json()
+}
+const data = await loadData()
 
 const timeTracks = document.querySelectorAll('.time-track')
-
 let checkedTrack = 'weekly';
 
 function removeActiveTrack() {
